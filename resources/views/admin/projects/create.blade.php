fdccf7dee7249360fe62463f22be549df03b2967
Francesca-Barcella <pollon.fra@gmail>
2023-01-11 16:28:03 +0100

    add edit

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -3,25 +3,51 @@
 @section('content')
 
 <div class="container">
-    <h1>Create New Project</h1>
-    <form action="{{route('admin.projects.store')}}" method="post">
-        @csrf
-        
-        <div class="mb-3">
-          <label for="title" class="form-label">Name</label>
-          <input type="text" name="title" id="title" class="form-control" placeholder="Add New Project" aria-describedby="titleHelpId">
-          <small id="titleHelpId" class="text-muted">Add new project</small>
-        </div>
+  <h1>Create New Project</h1>
 
-      <div class="mb-3">
-        <label for="description" class="form-label">Description</label>
-        <textarea class="form-control" name="description" id="description" rows="5"></textarea>
-      </div>
+  <!-- inserisco messaggio per validazione fallita -->
+  @if ($errors->any())
+  <div class="alert alert-danger">
+    <ul>
+      @foreach ($errors->all() as $error)
+      <li>{{ $error }}</li>
+      @endforeach
+    </ul>
+  </div>
+  @endif
 
-      <button type="submit" class="btn btn-primary">Create</button>
+  <form action="{{route('admin.projects.store')}}" method="post">
+    @csrf
+    <!--CAMPO TITLE-->
+    <div class="mb-3">
+      <label for="title" class="form-label">Name</label>
+      <input type="text" name="title" id="title" class="form-control @error('title') is-invalid @enderror" placeholder="Add New Project" aria-describedby="titleHelpId" value="{{old('title')}}">
+      <small id="titleHelpId" class="text-muted">Add new project</small>
+    </div>
 
-    </form>
+    <!-- messaggio di errore direttamente sotto al campo title -->
+    @error('title')
+    <div class="alert alert-danger" role="alert">
+      {{$message}}
+    </div>
+    @enderror
+
+    <!--CAMPO DESCRIPTION-->
+    <div class="mb-3">
+      <label for="description" class="form-label">Description</label>
+      <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="description" rows="5">{{old('description')}}</textarea>
+    </div>
+
+    <!-- messaggio di errore direttamente sotto al campo description -->
+    @error('description')
+    <div class="alert alert-danger" role="alert">
+      {{$message}}
+    </div>
+    @enderror
+
+    <button type="submit" class="btn btn-primary">Create</button>
+
+  </form>
 </div>
-    
 
 @endsection
--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -3,28 +3,51 @@
 @section('content')
 
 <div class="container">
-    <h1>Edit Project</h1>
-    <form action="{{route('admin.projects.store')}}" method="POST">
-        @csrf
-       
+  <h1>Create New Project</h1>
 
-        <div class="mb-3">
-          <label for="title" class="form-label">Name</label>
-          <input type="text" name="title" id="title" class="form-control" placeholder="Add New Project" aria-describedby="titleHelpId" value="{{old('title', $item->title)}}">
-          <small id="titleHelpId" class="text-muted">Add new project</small>
-        </div>
+  <!-- inserisco messaggio per validazione fallita -->
+  @if ($errors->any())
+  <div class="alert alert-danger">
+    <ul>
+      @foreach ($errors->all() as $error)
+      <li>{{ $error }}</li>
+      @endforeach
+    </ul>
+  </div>
+  @endif
 
-      <div class="mb-3">
-        <label for="description" class="form-label" value="{{old('decription', $item->decription)}}">Description</label>
-        <textarea class="form-control" name="description" id="description" rows="5"></textarea>
-      </div>
+  <form action="{{route('admin.projects.store')}}" method="post">
+    @csrf
+    <!--CAMPO TITLE-->
+    <div class="mb-3">
+      <label for="title" class="form-label">Name</label>
+      <input type="text" name="title" id="title" class="form-control @error('title') is-invalid @enderror" placeholder="Add New Project" aria-describedby="titleHelpId" value="{{old('title', $project->title)}}">
+      <small id="titleHelpId" class="text-muted">Add new project</small>
+    </div>
 
-      <button type="submit" class="btn btn-primary">Create</button>
+    <!-- messaggio di errore direttamente sotto al campo title -->
+    @error('title')
+    <div class="alert alert-danger" role="alert">
+      {{$message}}
+    </div>
+    @enderror
 
-    </form>
+    <!--CAMPO DESCRIPTION-->
+    <div class="mb-3">
+      <label for="description" class="form-label">Description</label>
+      <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="description" rows="5">{{old('description', $project->description)}}</textarea>
+    </div>
+
+    <!-- messaggio di errore direttamente sotto al campo description -->
+    @error('description')
+    <div class="alert alert-danger" role="alert">
+      {{$message}}
+    </div>
+    @enderror
+
+    <button type="submit" class="btn btn-primary">Create</button>
+
+  </form>
 </div>
-    
-
-
 
 @endsection

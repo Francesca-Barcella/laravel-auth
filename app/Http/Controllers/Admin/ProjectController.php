48b99a72e0db4f41f3a18cac6973064e938d047e
Francesca-Barcella <pollon.fra@gmail>
2023-01-13 16:19:15 +0100

    delete cover_image

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -137,6 +137,10 @@ class ProjectController extends Controller
      */
     public function destroy(Project $project)
     {
+        if ($project->cover_image) {
+            Storage::delete($project->cover_image);
+        }
+
         $project->delete();
         return to_route('admin.projects.index')->with('message', 'Post Delete Successfully');
     }
